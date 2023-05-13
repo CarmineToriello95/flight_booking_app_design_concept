3dd44e0f6b54cc860b7a19b3e2e1159888247c24
CarmineToriello95 <carminetoriello95@gmail.com>
2023-05-13 20:17:08 +0200

    Small refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:emirates_airlines_concept_ui/login_page.dart';
+import 'package:emirates_airlines_concept_ui/my_flights_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      home: const MyFlightsPage(),
     );
   }
 }
