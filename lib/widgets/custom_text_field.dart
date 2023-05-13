dc53cf91dbfe3edcd4d251aaa19489f5c4cedd0a
CarmineToriello95 <carminetoriello95@gmail.com>
2023-05-13 20:09:00 +0200

    Extracted custom text field widget

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final Icon prefixIcon;
+  final Color mainColor;
+  final Color secondaryColor;
+  final bool isTextObscure;
+
+  const CustomTextField({
+    Key? key,
+    required this.labelText,
+    required this.prefixIcon,
+    required this.mainColor,
+    required this.secondaryColor,
+    this.isTextObscure = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => TextField(
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          labelText: labelText,
+          labelStyle: TextStyle(color: secondaryColor),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: secondaryColor),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: mainColor),
+          ),
+        ),
+        cursorColor: mainColor,
+        obscureText: isTextObscure,
+        style: TextStyle(color: mainColor),
+      );
+}
